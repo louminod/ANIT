9e490568b37800c37a188f847f95dc853641416d
Levasseur Sébastien <sebastien.levasseur@efrei.net>
2022-01-12 10:38:38 +0100

    Added tests

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,6 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello ANIT");
-        Graph graph = new Graph("resources/k1.json");
 
         List<Object> formula = CTLParser.parseStringFormula("!(a\\/b)");
 
