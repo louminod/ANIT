0991e7097353c29e53d6164838683dcdcf0f46f8
Louis-Marie <louismarie.merminod@gmail.com>
2022-01-19 08:36:12 +0100

    Add not

--- src/Main.java
+++ src/Main.java
@@ -19,79 +19,11 @@ public class Main {
 
             //System.out.println(parsedFormula);
 
-            System.out.printf("RESULT -> %b", !run(graph, parsedFormula).isEmpty());
+            System.out.printf("RESULT -> %b", !Algorithmer.run(graph, parsedFormula).isEmpty());
         } else {
             System.out.println("Formula is not valid");
         }
     }
 
-    private static List<State> run(Graph graph, List<Object> formula) {
 
-        Operator operator = (Operator) formula.get(0);
-
-        Object first = null;
-        if (formula.get(1) instanceof ArrayList) {
-            first = !run(graph, (List<Object>) formula.get(1)).isEmpty();
-        } else {
-            first = (String) formula.get(1);
-        }
-
-        Object second = null;
-        if (formula.size() == 3) {
-            if (formula.get(2) instanceof ArrayList) {
-                second = !run(graph, (List<Object>) formula.get(2)).isEmpty();
-            } else {
-                second = (String) formula.get(2);
-            }
-        }
-
-        System.out.printf("operator -> %s | first -> %s | second -> %s\n", operator.getOperator(), first, second);
-
-        List<State> result = new ArrayList<>();
-        switch (operator.getOperator()) {
-            case "/\\":
-                if (first instanceof Boolean && second instanceof Boolean) {
-                    if ((Boolean) first && (Boolean) second) {
-                        result.add(new State("result"));
-                    }
-                } else if (first instanceof Boolean) {
-                    if ((Boolean) first && !Algorithmer.marking(graph, (String) second).isEmpty()) {
-                        result.add(new State("result"));
-                    }
-                } else if (second instanceof Boolean) {
-                    if (!Algorithmer.marking(graph, (String) first).isEmpty() && (Boolean) second) {
-                        result.add(new State("result"));
-                    }
-                } else {
-                    result = Algorithmer.and(graph, (String) first, (String) second);
-                }
-            case "\\/":
-                if (first instanceof Boolean && second instanceof Boolean) {
-                    if ((Boolean) first || (Boolean) second) {
-                        result.add(new State("result"));
-                    }
-                } else if (first instanceof Boolean) {
-                    if ((Boolean) first || !Algorithmer.marking(graph, (String) second).isEmpty()) {
-                        result.add(new State("result"));
-                    }
-                } else if (second instanceof Boolean) {
-                    if (!Algorithmer.marking(graph, (String) first).isEmpty() || (Boolean) second) {
-                        result.add(new State("result"));
-                    }
-                } else {
-                    result = Algorithmer.or(graph, (String) first, (String) second);
-                }
-            case "!":
-                if (first instanceof Boolean) {
-                    if (!(Boolean) first) {
-                        result.add(new State("result"));
-                    }
-                } else {
-                    if (!Algorithmer.marking(graph, (String) first).isEmpty()) {
-                        result.add(new State("result"));
-                    }
-                }
-        }
-        return result;
-    }
 }
