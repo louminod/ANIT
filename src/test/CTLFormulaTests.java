9c88fe41234cae80954a19129ec22a6032d37648
Louis-Marie <louismarie.merminod@gmail.com>
2022-01-25 11:30:16 +0100

    Fix tests

--- src/test/CTLFormulaTests.java
+++ src/test/CTLFormulaTests.java
@@ -22,11 +22,11 @@ public class CTLFormulaTests {
         assertEquals(Operator.class, result.get(0).getClass());
         assertEquals(ArrayList.class, result.get(1).getClass());
         assertEquals(Operator.class, ((ArrayList<Object>) result.get(1)).get(0).getClass());
-        assertEquals("Q3", ((ArrayList<Object>) result.get(1)).get(1));
+        assertEquals("[Q3]", ((ArrayList<Object>) result.get(1)).get(1).toString());
         assertEquals(ArrayList.class, ((ArrayList<Object>) result.get(1)).get(2).getClass());
         assertEquals(Operator.class, ((ArrayList) ((ArrayList<Object>) result.get(1)).get(2)).get(0).getClass());
-        assertEquals("Q1", ((ArrayList) ((ArrayList<Object>) result.get(1)).get(2)).get(1));
-        assertEquals("Q2", ((ArrayList) ((ArrayList<Object>) result.get(1)).get(2)).get(2));
+        assertEquals("[Q1]", ((ArrayList) ((ArrayList<Object>) result.get(1)).get(2)).get(1).toString());
+        assertEquals("[Q2]", ((ArrayList) ((ArrayList<Object>) result.get(1)).get(2)).get(2).toString());
     }
 
     @Test
@@ -36,8 +36,8 @@ public class CTLFormulaTests {
         List<Object> result = CTLParser.parseCTLFormula(parsedFormula);
 
         assertEquals(Operator.class, result.get(0).getClass());
-        assertEquals("a", result.get(1));
-        assertEquals("b", result.get(2));
+        assertEquals("[a]", result.get(1).toString());
+        assertEquals("[b]", result.get(2).toString());
     }
 
     @Test
@@ -47,8 +47,8 @@ public class CTLFormulaTests {
         List<Object> result = CTLParser.parseCTLFormula(parsedFormula);
 
         assertEquals(Operator.class, result.get(0).getClass());
-        assertEquals(Operator.class, result.get(1).getClass());
-        assertEquals("a", result.get(2));
+        assertEquals(ArrayList.class, result.get(1).getClass());
+        assertEquals("[a]", ((ArrayList) result.get(1)).get(1).toString());
     }
 
     @Test
