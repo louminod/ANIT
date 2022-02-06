3a33d2bc5dd6cf96402aac02aae7a0922fad9c00
Louis-Marie <louismarie.merminod@gmail.com>
2022-02-06 21:34:57 +0100

    Final commit

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,13 @@
 import algorithms.Algorithmer;
-import models.Operator;
 import models.kripke.Graph;
-import models.kripke.State;
 import tools.CTLParser;
-
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Graph graph = new Graph("resources/k2.json");
 
-        String input = "(a/\\b)\\/c";
+        String input = "AX(b\\/(EF%c))";
         System.out.println("Evaluating " + input);
         List<Object> formula = CTLParser.parseStringFormula(input);
 
